f515e2dee41414ba6bc89724399ea24b7f8f7325
sylvia villegas <sylvia@svillegas.com>
2017-01-24 14:03:13 -0500

    Add post route for new contact

--- crm.rb
+++ crm.rb
@@ -1,12 +1,6 @@
 require_relative "contact"
 require "sinatra"
 
-# TEST
-jose = Contact.create("Jose", "Bautista", "joeybats19@fake.email", "Flippin' bats for your playoff enjoyment!")
-edwin = Contact.create("Edwin", "Encarnacion", "edwin@fake.email", "Walking the parrot...in Cleveland. So sad. :\(")
-pillar = Contact.create("Kevin", "Pillar", "kevin.pillar@fake.email", "Catch all the things!")
-tulo = Contact.create("Troy", "Tulowitzki", "tulo@fake.email", "Nothing gets by this guy!")
-stro = Contact.create("Marcus", "Stroman", "stro@fake.email", "Welcome to the Stro-show! HDMH.")
 
 get "/" do
   @crm_app_name = "sylvia's CRM"
@@ -19,4 +13,9 @@ end
 
 get "/contacts/new" do
   erb :new_contact
+end
+
+post "/contacts" do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
