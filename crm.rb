a9a531b3893a188d1947371e8fc29b71e362a081
sylvia villegas <sylvia@svillegas.com>
2017-01-25 15:25:59 -0500

    Add post route for edit_contact

--- crm.rb
+++ crm.rb
@@ -37,3 +37,17 @@ get '/contacts/:id/edit' do
     raise Sinatra::NotFound
   end
 end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
