13aba5293d1ca5b53b32b6da47d074e035d991d7
sylvia villegas <sylvia@svillegas.com>
2017-01-25 12:08:59 -0500

    Create layouts; add styles

--- crm.rb
+++ crm.rb
@@ -1,7 +1,15 @@
 require_relative "contact"
 require "sinatra"
 
+# TEST DATA
+jose = Contact.create("Jose", "Bautista", "joeybats19@fake.email", "Flippin' bats for your playoff enjoyment!")
+edwin = Contact.create("Edwin", "Encarnacion", "edwin@fake.email", "Walking the parrot...in Cleveland. So sad. :\(")
+pillar = Contact.create("Kevin", "Pillar", "kevin.pillar@fake.email", "Catch all the things!")
+tulo = Contact.create("Troy", "Tulowitzki", "tulo@fake.email", "Nothing gets by this guy!")
+stro = Contact.create("Marcus", "Stroman", "stro@fake.email", "Welcome to the Stro-show! HDMH.")
 
+
+# ROUTES
 get "/" do
   @crm_app_name = "sylvia's CRM"
   erb :index
