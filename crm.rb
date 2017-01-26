642e2da507b4c7f6362ab4dd6805d5111a23121d
sylvia villegas <sylvia@svillegas.com>
2017-01-26 12:44:54 -0500

    Change attribute format in post '/contacts'

--- crm.rb
+++ crm.rb
@@ -16,7 +16,12 @@ get "/contacts/new" do
 end
 
 post "/contacts" do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
   redirect to('/contacts')
 end
 
